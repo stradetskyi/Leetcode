2eb6691625e6a9986bc5defbb832604928c30f45
Alex Stradetskyi <lexsop@gmail.com>
2018-12-12 22:23:17 +0200

    - Add ReverseInteger https://leetcode.com/problems/reverse-integer/

---
+++ Leetcode.Issues/ReverseInteger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Leetcode.Issues
+{
+    public class ReverseInteger
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/reverse-integer/
+        /// </summary>
+        public int Reverse(int x)
+        {
+            Stack<int> numbers = new Stack<int>();
+
+            int n = x;
+            while (n != 0)
+            {
+                decimal d = n;
+                decimal newD = d / 10;
+                int truncate = (int)Math.Truncate(newD);
+                int minedNum = (int)((newD + (truncate * (-1))) * 10);
+
+                numbers.Push(minedNum);
+
+                n = truncate;
+            }
+
+            int newX = 0;
+            int pos = 1;
+            while (numbers.Count > 0)
+            {
+                int num = numbers.Pop();
+                if (pos == 1000000000 && (newX > 147483647 || newX < -147483648) && Math.Abs(num) >= 2)
+                {
+                    newX = 0;
+                }
+                else
+                {
+                    newX = newX + num * pos;
+                }
+
+                pos = pos * 10;
+            }
+            return newX;
+        }
+    }
+}
---
+++ Leetcode.Tests/ReverseIntegerTests.cs
@@ -0,0 +1,26 @@
+﻿using Leetcode.Issues;
+using NUnit.Framework;
+
+namespace Leetcode.Tests
+{
+    public class ReverseIntegerTests
+    {
+        private ReverseInteger _solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            _solution = new ReverseInteger();
+        }
+
+        [Test]
+        [TestCase(123, 321)]
+        [TestCase(-123, -321)]
+        [TestCase(120, 21)]
+        [TestCase(1534236469, 0)]
+        public void RunSolution_CheckPositiveCases_NoError(int x, int newX)
+        {
+            Assert.AreEqual(newX, _solution.Reverse(x));
+        }
+    }
+}
