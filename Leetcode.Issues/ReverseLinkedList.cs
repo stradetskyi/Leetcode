6429c77c2d43f3c8f00856852fb62957b556fc81
Oleksandr Stradetskyi <lexsop@gmail.com>
2023-03-24 18:40:15 +0200

    - Add Reverse Linked List https://leetcode.com/problems/reverse-linked-list/

---
+++ Leetcode.Issues/ReverseLinkedList.cs
@@ -0,0 +1,35 @@
+﻿namespace Leetcode.Issues
+{
+    /// <summary>
+    /// https://leetcode.com/problems/reverse-linked-list/
+    /// </summary>
+    public class ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            var current = head;
+            ListNode prev = null;
+            while (current != null)
+            {
+                var tmp = current.next;
+                current.next = prev;
+
+                prev = current;
+                current = tmp;
+            }
+
+            return prev;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ Leetcode.Tests/ReverseLinkedListTests.cs
@@ -0,0 +1,68 @@
+﻿using Leetcode.Issues;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Leetcode.Tests
+{
+    [TestFixture]
+    public class ReverseLinkedListTests
+    {
+        private ReverseLinkedList _solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            _solution = new ReverseLinkedList();
+        }
+
+        [Test]
+        [TestCase(new [] { 1, 2, 3, 4, 5 }, new [] { 5, 4, 3, 2, 1 })]
+        [TestCase(new [] { 1, 2 }, new [] { 2, 1 })]
+        [TestCase(new int[0], new int[0])]
+        public void RunSolution_ReverseList_NoError(int[] head, int[] result)
+        {
+            var headNode = head.CovertToListNode();
+            var resNode = _solution.ReverseList(headNode);
+
+            var resArray = resNode.CovertToArray();
+            for (int i = 0; i < result.Length; i++)
+            {
+                Assert.AreEqual(result[i], resArray[i]);
+            }
+        }
+    }
+
+    public static class Extension
+    {
+        public static ListNode CovertToListNode(this int[] head)
+        {
+            if (head == null || 
+                head.Length == 0) 
+                return null;
+
+            if (head.Length == 1)
+                return new ListNode(head[0]);
+
+            return new ListNode(head[0], head.SubArray(1, head.Length-1).CovertToListNode());
+        }
+
+        public static T[] SubArray<T>(this T[] data, int index, int length)
+        {
+            T[] result = new T[length];
+            Array.Copy(data, index, result, 0, length);
+            return result;
+        }
+
+        public static int[] CovertToArray(this ListNode head)
+        {
+            if (head == null)
+                return Array.Empty<int>();
+
+            var list = new List<int> { head.val };
+            list.AddRange(head.next?.CovertToArray() ?? Array.Empty<int>());
+
+            return list.ToArray();
+        }
+    }
+}
