61555a226c123a730b2b0c5a5037bbc2df80afe0
Oleksandr Stradetskyi <lexsop@gmail.com>
2023-06-23 23:09:41 +0300

    - Add 929. Unique Email Addresses https://leetcode.com/problems/unique-email-addresses/ Simple solution. Required memory and performance improvements

---
+++ Leetcode.Issues/UniqueEmailAddresses.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Leetcode.Issues
+{
+    /// <summary>
+    /// 929. Unique Email Addresses https://leetcode.com/problems/unique-email-addresses/
+    /// </summary>
+    public class UniqueEmailAddresses
+    {
+        public int NumUniqueEmails(string[] emails)
+        {
+            var uniqueEmails = new HashSet<string>();
+
+            foreach (var email in emails)
+            {
+                var emailParts = email.Split('@');
+                var localName = emailParts[0];
+                var domainName = emailParts[1];
+
+                var localNameParts = localName.Split('+');
+                var localNameWithoutPlus = localNameParts[0];
+                var localNameWithoutDots = localNameWithoutPlus.Replace(".", "");
+
+                var uniqueEmail = localNameWithoutDots + "@" + domainName;
+                uniqueEmails.Add(uniqueEmail);
+            }
+
+            return uniqueEmails.Count;
+        }
+    }
+}
---
+++ Leetcode.Tests/UniqueEmailAddressesTests.cs
@@ -0,0 +1,29 @@
+﻿using Leetcode.Issues;
+using NUnit.Framework;
+
+namespace Leetcode.Tests
+{
+    [TestFixture]
+    public class UniqueEmailAddressesTests
+    {
+        private UniqueEmailAddresses _solution;
+
+        [SetUp]
+        public void Setup()
+        {
+            _solution = new UniqueEmailAddresses();
+        }
+
+        [Test]
+        [TestCase(new[]
+        {
+            "test.email+alex@leetcode.com",
+            "test.e.mail+bob.cathy@leetcode.com",
+            "testemail+david@lee.tcode.com"
+        }, 2)]
+        public void RunSolution_CheckPositiveCases_NoError(string[] emails, int count)
+        {
+            Assert.AreEqual(count, _solution.NumUniqueEmails(emails));
+        }
+    }
+}
